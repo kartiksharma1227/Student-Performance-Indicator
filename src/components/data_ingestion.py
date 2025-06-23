12ffc1a15c149ca5f0f9147baccfc31863765765
Kartik Sharma <kartiksharma1227@gmail.com>
2025-06-23 18:43:44 +0530

    Model Training With hyper parameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,5 @@
 import os
+from pyexpat import model
 import sys
 from src.exception import CustomException
 from src.logger import logging
@@ -9,6 +10,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -52,6 +55,9 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
-    logging.info("Data ingestion completed successfully.")
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+    # logging.info("Data ingestion completed successfully.")
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
     
